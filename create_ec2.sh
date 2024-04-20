1fc367bf35c56d1e1590899284bb9a5d81ada9f7
tharunks <srinivastharun123@gmail.com>
2024-04-20 19:13:33 +0530

    added ec2.sh

--- create_ec2.sh
+++ create_ec2.sh
@@ -14,7 +14,7 @@ do
 	INSTANCE_TYPE="t2.micro"
 	fi
 	echo "Creating $i instance"
-	IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+	IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress'))
 echo "created $i instance: $IP_ADDRESS"
 
 $ aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
