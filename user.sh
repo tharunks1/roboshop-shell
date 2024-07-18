759fb25e00fce87cb4a3507ba2fc47b80d0db96b
SRINIVAS_THARUN <88927252+tharunks1@users.noreply.github.com>
2024-07-18 21:37:02 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -100,6 +100,6 @@ dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB client"
 
-mongo --host  mongodb.chittirobo.shop  </app/schema/user.js &>> $LOGFILE
+mongo --host  mongodb.chittirobo.shop  </app/schema/user.js 
 
 VALIDATE $? "Loading user data into MongoDB"
